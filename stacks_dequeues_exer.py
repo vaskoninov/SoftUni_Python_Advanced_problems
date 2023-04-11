5d950154f81c3558f64f1604d76e0ff10e37f128
vaskoninov <vasko.ninov@gmail.com>
2023-04-11 09:55:11 +0300

    new project

--- stacks_dequeues_exer.py
+++ stacks_dequeues_exer.py
@@ -122,3 +122,102 @@
 #     print("NO")
 
 ######################
+# from collections import deque
+#
+# info_robots = input().split(";")
+# robots = {}
+# for info in info_robots:
+#     info = info.split("-")
+#     name = info[0]
+#     time = int(info[1])
+#     robots[name] = {}
+#     robots[name]['time'] = time
+#     robots[name]['is_working'] = False
+#     robots[name]['counter'] = 0
+#
+# times = [int(x) for x in input().split(":")]
+#
+# products = deque()
+#
+# while True:
+#     product = input()
+#     if product == "End":
+#         break
+#     products.append(product)
+#
+# while products:
+#     for key, values in robots.items():
+#         if values['is_working']:
+#             values['counter'] += 1
+#     times[2] += 1
+#     if times[2] == 60:
+#         times[2] = 0
+#         times[1] += 1
+#         if times[1] == 60:
+#             times[1] = 0
+#             times[0] += 1
+#             if times[0] == 24:
+#                 times[0] = 0
+#
+#     for key, values in robots.items():
+#         if values['counter'] % values['time'] == 0:
+#             values["is_working"] = False
+#
+#     to_work = products.popleft()
+#
+#     for robot, values in robots.items():
+#         if not values['is_working']:
+#             values['is_working'] = True
+#             name = robot
+#             print(f"{name} - {to_work} [{times[0]:02}:{times[1]:02}:{times[2]:02}]")
+#             break
+#     else:
+#         products.append(to_work)
+
+####################
+# from collections import deque
+#
+# green_duration = int(input())
+# free_window = int(input())
+#
+# cars = deque()
+# counter = 0
+# passed = []
+# safety = True
+#
+# while safety:
+#     commands = input()
+#     if commands == "END":
+#         break
+#     if commands == "green":
+#         safety_time = green_duration
+#         while safety_time > 0:
+#             if cars:
+#                 passing = cars.popleft()
+#                 if len(passing) <= safety_time:
+#                     counter += 1
+#                     passed.append(passing)
+#                     safety_time -= len(passing)
+#                 else:
+#                     rest = abs(safety_time - len(passing))
+#                     if rest <= free_window:
+#                         counter += 1
+#                         passed.append(passing)
+#                         break
+#                     else:
+#                         hit = abs(free_window - rest)
+#                         char = passing[-hit]
+#                         print("A crash happened!")
+#                         print(f"{passing} was hit at {char}.")
+#                         safety = False
+#                         break
+#             else:
+#                 break
+#     else:
+#         cars.append(commands)
+#
+# if safety:
+#     print("Everyone is safe.")
+#     print(f"{counter} total cars passed the crossroads.")
+
+####################
