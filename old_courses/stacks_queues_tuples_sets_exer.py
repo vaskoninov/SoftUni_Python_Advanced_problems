e4986f9e96b4248d08960d7338a1a2a91ece9e37
vaskoninov <vasko.ninov@gmail.com>
2023-04-12 13:45:39 +0300

    new exercises

--- old_courses/stacks_queues_tuples_sets_exer.py
+++ old_courses/stacks_queues_tuples_sets_exer.py
@@ -108,36 +108,114 @@
 
 #################
 
-from collections import deque
-
-honey = 0
-bees = deque([int(x) for x in input().split()])
-nectar = [int(x) for x in input().split()]
-symbols = deque([x for x in input().split()])
-
-while bees and nectar:
-    result = 0
-    bee = bees.popleft()
-    gathered = nectar.pop()
-    if gathered < bee:
-        bees.appendleft(bee)
-        continue
-    operator = symbols.popleft()
-    if operator == "/":
-        if gathered > 0:
-            result = abs(bee / gathered)
-        else:
-            result = 0
-    if operator == "*":
-        result = abs(bee * gathered)
-    if operator == "+":
-        result = abs(bee + gathered)
-    if operator == "-":
-        result = abs(bee - gathered)
-    honey += result
-
-print(f"Total honey made: {honey}")
-if bees:
-    print(f"Bees left: {', '.join(map(str, bees))}")
-if nectar:
-    print(f"Nectar left: {', '.join(map(str, nectar))}")
+# from collections import deque
+#
+# honey = 0
+# bees = deque([int(x) for x in input().split()])
+# nectar = [int(x) for x in input().split()]
+# symbols = deque([x for x in input().split()])
+#
+# while bees and nectar:
+#     result = 0
+#     bee = bees.popleft()
+#     gathered = nectar.pop()
+#     if gathered < bee:
+#         bees.appendleft(bee)
+#         continue
+#     operator = symbols.popleft()
+#     if operator == "/":
+#         if gathered > 0:
+#             result = abs(bee / gathered)
+#         else:
+#             result = 0
+#     if operator == "*":
+#         result = abs(bee * gathered)
+#     if operator == "+":
+#         result = abs(bee + gathered)
+#     if operator == "-":
+#         result = abs(bee - gathered)
+#     honey += result
+#
+# print(f"Total honey made: {honey}")
+# if bees:
+#     print(f"Bees left: {', '.join(map(str, bees))}")
+# if nectar:
+#     print(f"Nectar left: {', '.join(map(str, nectar))}")
+###################
+# from collections import deque
+#
+# materials = [int(x) for x in input().split()]
+# magic = deque([int(x) for x in input().split()])
+#
+# presents = {
+#     "Doll": {
+#         'built': False,
+#         'quantity': 0,
+#     },
+#     "Wooden train": {
+#         'built': False,
+#         'quantity': 0,
+#     },
+#     "Teddy bear": {
+#         'built': False,
+#         'quantity': 0,
+#     },
+#     "Bicycle": {
+#         'built': False,
+#         'quantity': 0,
+#     },
+# }
+#
+# while materials and magic:
+#     box = materials.pop()
+#     spell = magic.popleft()
+#
+#     if box == 0 and spell == 0:
+#         continue
+#     if box == 0:
+#         magic.appendleft(spell)
+#         continue
+#     if spell == 0:
+#         materials.append(box)
+#         continue
+#
+#     value = box * spell
+#
+#     if value < 0:
+#         amount = box + spell
+#         materials.append(amount)
+#         continue
+#
+#     if value == 150:
+#         presents["Doll"]['built'] = True
+#         presents["Doll"]['quantity'] += 1
+#     elif value == 250:
+#         presents["Wooden train"]['built'] = True
+#         presents["Wooden train"]['quantity'] += 1
+#     elif value == 300:
+#         presents["Teddy bear"]['built'] = True
+#         presents["Teddy bear"]['quantity'] += 1
+#     elif value == 400:
+#         presents["Bicycle"]['built'] = True
+#         presents["Bicycle"]['quantity'] += 1
+#     else:
+#         box += 15
+#         materials.append(box)
+#
+# if (presents["Doll"]['built'] and presents["Wooden train"]['built']) or \
+#         (presents["Bicycle"]['built'] and presents["Teddy bear"]['built']):
+#     print("The presents are crafted! Merry Christmas!")
+# else:
+#     print("No presents this Christmas!")
+#
+# if materials:
+#     print(f"Materials left: {', '.join(map(str, reversed(materials)))}")
+# if magic:
+#     print(f"Magic left: {', '.join(map(str, magic))}")
+#
+# sorted_presents = dict(sorted(presents.items()))
+#
+# for key, values in sorted_presents.items():
+#     if values['quantity'] > 0:
+#         print(f"{key}: {values['quantity']}")
+###################
