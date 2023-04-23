6fd5d78269f9d07bfd6c55b5281ed16b2a700795
vaskoninov <vasko.ninov@gmail.com>
2023-04-23 18:25:10 +0300

    new exercises

--- old_courses/functions_advanced.py
+++ old_courses/functions_advanced.py
@@ -120,19 +120,76 @@
 #
 # print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
 
-def sorting_cheeses(**kwargs):
-    for key in kwargs:
-        kwargs[key] = sorted(kwargs[key], reverse=True)
+# def sorting_cheeses(**kwargs):
+#     for key in kwargs:
+#         kwargs[key] = sorted(kwargs[key], reverse=True)
+#
+#     sorted_cheese = dict(sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0])))
+#
+#     result = []
+#     for cheese, value in sorted_cheese.items():
+#         result.append(cheese)
+#         for v in value:
+#             result.append(str(v))
+#
+#     return "\n".join(result)
+# print(sorting_cheeses(Parmesan=[102, 120, 135], Camembert=[100, 100, 105, 500, 430], Mozzarella=[50, 125], ))
 
-    sorted_cheese = dict(sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0])))
-
-    result = []
-    for cheese, value in sorted_cheese.items():
-        result.append(cheese)
-        for v in value:
-            result.append(str(v))
-
-    return "\n".join(result)
+#############
 
 
-print(sorting_cheeses(Parmesan=[102, 120, 135], Camembert=[100, 100, 105, 500, 430], Mozzarella=[50, 125], ))
+# def rectangle(a, b):
+#     if not isinstance(a, int) or not isinstance(b, int):
+#         return "Enter valid values!"
+#
+#     def area(a, b):
+#         return a * b
+#
+#     def perimeter(a, b):
+#         return 2 * a + 2 * b
+#
+#     return f"Rectangle area: {area(a, b)}\nRectangle perimeter: {perimeter(a, b)}"
+#
+#
+# print(rectangle(2, 10))
+# print(rectangle("a", 10))
+
+###############
+
+def operate(operator, *args):
+    def addition(args):
+        result = 0
+        for i in args:
+            result += i
+        return result
+
+    def multiply(args):
+        result = 1
+        for i in args:
+            result *= i
+        return result
+
+    def division(args):
+        result = args[0]
+        for i in args[1:]:
+            result /= i
+        return result
+
+    def subtractions(args):
+        result = args[0]
+        for i in args[1:]:
+            result -= i
+        return result
+
+    if operator == "+":
+        return addition(args)
+    elif operator == "-":
+        return subtractions(args)
+    elif operator == "*":
+        return multiply(args)
+    elif operator == "/":
+        return division(args)
+
+
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
