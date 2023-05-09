91237e8956745478fbd840066fc8764970b391cb
vaskoninov <vasko.ninov@gmail.com>
2023-05-09 20:17:37 +0300

    new exercises

--- May2023/stacks_and_queues_lab.py
+++ May2023/stacks_and_queues_lab.py
@@ -39,17 +39,80 @@
 
 ######## Supermarket ##########
 
+# from collections import deque
+#
+# line = deque()
+#
+# while True:
+#     name = input()
+#     if name == "End":
+#         print(f"{len(line)} people remaining.")
+#         break
+#     if name == "Paid":
+#         while len(line) > 0:
+#             print(line.popleft())
+#         continue
+#     line.append(name)
+
+######## Water Dispenser ###########
+# from collections import deque
+#
+# water = int(input())
+#
+# people = deque()
+#
+# while True:
+#     line = input()
+#     if line == "Start":
+#         break
+#     people.append(line)
+#
+# while True:
+#     line = input()
+#     if line == "End":
+#         print(f"{water} liters left")
+#         break
+#     instructions = line.split()
+#     if instructions[0] == "refill":
+#         water += int(instructions[1])
+#     else:
+#         thirst = int(instructions[0])
+#         if thirst <= water:
+#             water -= thirst
+#             print(f"{people.popleft()} got water")
+#         else:
+#             print(f"{people.popleft()} must wait")
+
+##### Hot Potato ########
+#
+# from collections import deque
+#
+#
+# def hot_potato(sequence, n):
+#     counter = 1
+#     while counter != n:
+#         name = sequence.popleft()
+#         counter += 1
+#         sequence.append(name)
+#     return f"Removed {sequence.popleft()}"
+#
+#
+# kids = deque(input().split())
+# rotation = int(input())
+#
+# while len(kids) > 1:
+#     print(hot_potato(kids, rotation))
+# print(f"Last is {kids[0]}")
+
+#### Hot Potato 2 ######
+
 from collections import deque
 
-line = deque()
+dd = deque(input().split())
+n = int(input())
 
-while True:
-    name = input()
-    if name == "End":
-        print(f"{len(line)} people remaining.")
-        break
-    if name == "Paid":
-        while len(line) > 0:
-            print(line.popleft())
-        continue
-    line.append(name)
+while len(dd) > 1:
+    dd.rotate(-n + 1)
+    print(f"Removed {dd.popleft()}")
+
+print(f"Last is {dd[0]}")
--- tryouts/collections_tryouts.py
+++ tryouts/collections_tryouts.py
@@ -1,5 +1,13 @@
-from collections import Counter
+# name = "Barbara"
+# c = Counter(name.lower())
+# print(c)
+from collections import deque
 
-name = "Barbara"
-c = Counter(name.lower())
-print(c)
+dd = deque(["George", "Peter", "Michael", "William", "Thomas"])
+n = 10
+
+while len(dd) > 1:
+    dd.rotate(-n + 1)
+    print(f"Removed {dd.popleft()}")
+
+print(f"Last is {dd[0]}")
