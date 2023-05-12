50bc4e0b16a2eb5a340a1c5cc64ee640d7a0dbe0
Vasil Ninov <30572741+vaskoninov@users.noreply.github.com>
2023-05-12 06:36:09 +0000

    new solutions

--- May2023/Exam_Preparation/april_2023.py
+++ May2023/Exam_Preparation/april_2023.py
@@ -34,52 +34,87 @@
 
 ##### The Squirrel
 
-def is_valid(n, m):
-    if n in range(size) and m in range(size):
-        return True
+# def is_valid(n, m):
+#     if n in range(size) and m in range(size):
+#         return True
 
 
-movements = {
-    "left": (0, -1),
-    "right": (0, 1),
-    "up": (-1, 0),
-    "down": (1, 0),
-}
-nuts = 0
+# movements = {
+#     "left": (0, -1),
+#     "right": (0, 1),
+#     "up": (-1, 0),
+#     "down": (1, 0),
+# }
+# nuts = 0
 
-size = int(input())
-squirrel = None
-directions = [x for x in input().split(", ")]
-matrix = []
+# size = int(input())
+# squirrel = None
+# directions = [x for x in input().split(", ")]
+# matrix = []
 
-for i in range(size):
-    row = list(input())
-    if "s" in row:
-        squirrel = (i, row.index("s"))
-    matrix.append(row)
+# for i in range(size):
+#     row = list(input())
+#     if "s" in row:
+#         squirrel = (i, row.index("s"))
+#     matrix.append(row)
 
-for direction in directions:
-    to_move = (squirrel[0] + movements[direction][0], squirrel[1] + movements[direction][1])
-    if is_valid(to_move[0], to_move[1]):
-        matrix[squirrel[0]][squirrel[1]] = "*"
-        squirrel = (to_move[0], to_move[1])
-        if matrix[to_move[0]][to_move[1]] == "t":
-            print("Unfortunately, the squirrel stepped on a trap...")
-            print(f"Hazelnuts collected: {nuts}")
-            break
-        if matrix[to_move[0]][to_move[1]] == "h":
-            nuts += 1
-            matrix[to_move[0]][to_move[1]] = "*"
-            if nuts == 3 :
-                print("Good job! You have collected all hazelnuts!")
-                print(f"Hazelnuts collected: {nuts}")
-                break
-        if matrix[to_move[0]][to_move[1]] == "*":
-            continue
-    else:
-        print("The squirrel is out of the field.")
-        print(f"Hazelnuts collected: {nuts}")
-        break
-else:
-    print("There are more hazelnuts to collect.")
-    print(f"Hazelnuts collected: {nuts}")
+# for direction in directions:
+#     to_move = (squirrel[0] + movements[direction][0], squirrel[1] + movements[direction][1])
+#     if is_valid(to_move[0], to_move[1]):
+#         matrix[squirrel[0]][squirrel[1]] = "*"
+#         squirrel = (to_move[0], to_move[1])
+#         if matrix[to_move[0]][to_move[1]] == "t":
+#             print("Unfortunately, the squirrel stepped on a trap...")
+#             print(f"Hazelnuts collected: {nuts}")
+#             break
+#         if matrix[to_move[0]][to_move[1]] == "h":
+#             nuts += 1
+#             matrix[to_move[0]][to_move[1]] = "*"
+#             if nuts == 3 :
+#                 print("Good job! You have collected all hazelnuts!")
+#                 print(f"Hazelnuts collected: {nuts}")
+#                 break
+#         if matrix[to_move[0]][to_move[1]] == "*":
+#             continue
+#     else:
+#         print("The squirrel is out of the field.")
+#         print(f"Hazelnuts collected: {nuts}")
+#         break
+# else:
+#     print("There are more hazelnuts to collect.")
+#     print(f"Hazelnuts collected: {nuts}")
+
+##### Movie Organizer
+
+def movie_organizer(*args):
+    movies = {}
+    result = []
+    for entry in args:
+        if entry[1] not in movies:
+            movies[entry[1]] = []
+        movies[entry[1]].append(entry[0])
+
+    sorted_movies = dict(sorted(movies.items(), key=lambda x: (-len(x[1]), x[0])))
+
+
+    for key, values in sorted_movies.items():
+        sorted_movies[key] = values.sort()
+        result.append(f"{key} - {len(values)}")
+        for entry in values:
+            result.append(f"* {entry}")
+    
+    return "\n".join(result)
+
+    
+
+print(movie_organizer(("Avatar: The Way of Water", "Action"),
+    ("House Of Gucci", "Drama"),
+    ("Top Gun", "Action"),
+    ("Ted", "Comedy"),
+    ("Duck Soup", "Comedy"),
+    ("The Dark Knight", "Action"),
+    ("A Star Is Born", "Musicals"),
+    ("The Warrior", "Action"),
+    ("Like A Boss", "Comedy"),
+    ("The Green Mile", "Drama"),
+    ("21 Jump Street", "Comedy")))
