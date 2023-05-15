1e0784d852cd7ee99df80553c5f64065b9e1d7b0
vaskoninov <vasko.ninov@gmail.com>
2023-05-15 20:08:06 +0300

    new exercises

--- May2023/stacks_queues_tuples_sets_exercises.py
+++ May2023/stacks_queues_tuples_sets_exercises.py
@@ -22,45 +22,212 @@
 # print(", ".join(map(str, sorted(set_two))))
 
 #### Milkshakes ######
+#
+# from collections import deque
+#
+# chocolates = deque([int(x) for x in input().split(", ")])
+# milk = deque([int(x) for x in input().split(", ")])
+# milkshakes = 0
+#
+# while chocolates and milk:
+#     choco = chocolates.pop()
+#     cup = milk.popleft()
+#
+#     if choco <= 0 and cup <= 0:
+#         continue
+#     if choco <= 0:
+#         milk.appendleft(cup)
+#         continue
+#     if cup <= 0:
+#         chocolates.append(choco)
+#         continue
+#
+#     if choco == cup:
+#         milkshakes += 1
+#         if milkshakes == 5:
+#             break
+#         continue
+#     choco -= 5
+#     milk.append(cup)
+#     chocolates.append(choco)
+#
+# if milkshakes == 5:
+#     print("Great! You made all the chocolate milkshakes needed!")
+# else:
+#     print("Not enough milkshakes.")
+#
+# if chocolates:
+#     print(f"Chocolate: {', '.join(map(str, chocolates))}")
+# else:
+#     print("Chocolate: empty")
+# if milk:
+#     print(f"Milk: {', '.join(map(str, milk))}")
+# else:
+#     print("Milk: empty")
+
+##### Expression Evaluator
+# from functools import reduce
+# from collections import deque
+#
+# operators = ["/", "+", "-", "*"]
+# line = deque(int(x) if x.lstrip('-').isdigit() else x for x in input().split())
+#
+# while len(line) > 1:
+#     temp = []
+#     operator = ""
+#     while True:
+#         num = line.popleft()
+#         if num in operators:
+#             operator = num
+#             break
+#         temp.append(num)
+#     if operator == "/":
+#         result = reduce(lambda a, b: a // b, temp)
+#     if operator == "*":
+#         result = reduce(lambda a, b: a * b, temp)
+#     if operator == "-":
+#         result = reduce(lambda a, b: a - b, temp)
+#     if operator == "+":
+#         result = reduce(lambda a, b: a + b, temp)
+#     line.appendleft(result)
+# print(line[0])
+
+#### Expression Evaluator Mapper
+#
+# from collections import deque
+# from functools import reduce
+#
+# operators = {
+#     "/": lambda x: reduce(lambda a, b: a // b, x),
+#     "+": lambda x: reduce(lambda a, b: a + b, x),
+#     "-": lambda x: reduce(lambda a, b: a - b, x),
+#     "*": lambda x: reduce(lambda a, b: a * b, x),
+# }
+#
+# line = deque(int(x) if x.lstrip('-').isdigit() else x for x in input().split())
+#
+# while len(line) > 1:
+#     temp = []
+#     operator = None
+#     while True:
+#         num = line.popleft()
+#         if not isinstance(num, int):
+#             operator = num
+#             break
+#         temp.append(num)
+#     result = operators[operator](temp)
+#     line.appendleft(result)
+#
+# print(line[0])
+
+
+##### Honey
+# from collections import deque
+#
+# func_mapper = {
+#     "/": lambda x, a: abs(x / a) if a > 0 else 0,
+#     "+": lambda x, a: abs(x + a),
+#     "*": lambda x, a: abs(x * a),
+#     "-": lambda x, a: abs(x - a),
+# }
+#
+# honey = 0
+# bees = deque([int(x) for x in input().split()])
+# nectar = [int(x) for x in input().split()]
+# symbols = deque([x for x in input().split()])
+#
+# while bees and nectar:
+#     bee = bees.popleft()
+#     gathered = nectar.pop()
+#     if gathered < bee:
+#         bees.appendleft(bee)
+#         continue
+#     operator = symbols.popleft()
+#     result = func_mapper[operator](bee, gathered)
+#
+#     honey += result
+#
+# print(f"Total honey made: {honey}")
+# if bees:
+#     print(f"Bees left: {', '.join(map(str, bees))}")
+# if nectar:
+#     print(f"Nectar left: {', '.join(map(str, nectar))}")
+
+#### Santa's Present Factory
 
 from collections import deque
 
-chocolates = deque([int(x) for x in input().split(", ")])
-milk = deque([int(x) for x in input().split(", ")])
-milkshakes = 0
+materials = [int(x) for x in input().split()]
+magic = deque([int(x) for x in input().split()])
 
-while chocolates and milk:
-    choco = chocolates.pop()
-    cup = milk.popleft()
+presents = {
+    "Doll": {
+        'built': False,
+        'quantity': 0,
+    },
+    "Wooden train": {
+        'built': False,
+        'quantity': 0,
+    },
+    "Teddy bear": {
+        'built': False,
+        'quantity': 0,
+    },
+    "Bicycle": {
+        'built': False,
+        'quantity': 0,
+    },
+}
 
-    if choco <= 0 and cup <= 0:
+while materials and magic:
+    box = materials.pop()
+    spell = magic.popleft()
+
+    if box == 0 and spell == 0:
         continue
-    if choco <= 0:
-        milk.appendleft(cup)
+    if box == 0:
+        magic.appendleft(spell)
         continue
-    if cup <= 0:
-        chocolates.append(choco)
+    if spell == 0:
+        materials.append(box)
         continue
 
-    if choco == cup:
-        milkshakes += 1
-        if milkshakes == 5:
-            break
-        continue
-    choco -= 5
-    milk.append(cup)
-    chocolates.append(choco)
+    value = box * spell
 
-if milkshakes == 5:
-    print("Great! You made all the chocolate milkshakes needed!")
+    if value < 0:
+        amount = box + spell
+        materials.append(amount)
+        continue
+
+    if value == 150:
+        presents["Doll"]['built'] = True
+        presents["Doll"]['quantity'] += 1
+    elif value == 250:
+        presents["Wooden train"]['built'] = True
+        presents["Wooden train"]['quantity'] += 1
+    elif value == 300:
+        presents["Teddy bear"]['built'] = True
+        presents["Teddy bear"]['quantity'] += 1
+    elif value == 400:
+        presents["Bicycle"]['built'] = True
+        presents["Bicycle"]['quantity'] += 1
+    else:
+        box += 15
+        materials.append(box)
+
+if (presents["Doll"]['built'] and presents["Wooden train"]['built']) or \
+        (presents["Bicycle"]['built'] and presents["Teddy bear"]['built']):
+    print("The presents are crafted! Merry Christmas!")
 else:
-    print("Not enough milkshakes.")
+    print("No presents this Christmas!")
 
-if chocolates:
-    print(f"Chocolate: {', '.join(map(str, chocolates))}")
-else:
-    print("Chocolate: empty")
-if milk:
-    print(f"Milk: {', '.join(map(str, milk))}")
-else:
-    print("Milk: empty")
+if materials:
+    print(f"Materials left: {', '.join(map(str, reversed(materials)))}")
+if magic:
+    print(f"Magic left: {', '.join(map(str, magic))}")
+
+sorted_presents = dict(sorted(presents.items()))
+
+for key, values in sorted_presents.items():
+    if values['quantity'] > 0:
+        print(f"{key}: {values['quantity']}")
