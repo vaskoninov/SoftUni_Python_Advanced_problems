a02a07a1cd4f5377c3dd268b28d9141dfb83d923
Vasil Ninov <30572741+vaskoninov@users.noreply.github.com>
2023-05-17 05:12:19 +0000

    s

---
+++ May2023/matrices_exercise_one.py
@@ -0,0 +1,10 @@
+#### Diagonals
+
+# matrix = [[int(x) for x in input().split(", ")] for _ in range(int(input()))]
+
+# primary_diagonal = [matrix[i][i] for i in range(len(matrix))]
+# secondary_diagonal = [matrix[i][len(matrix) - i - 1] for i in range(len(matrix))]
+
+# print(f"Primary diagonal: {', '.join([str(x) for x in primary_diagonal])}. Sum: {sum(primary_diagonal)}")
+# print(f"Secondary diagonal: {', '.join([str(x) for x in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
+
