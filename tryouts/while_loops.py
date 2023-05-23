eb89f1afb1db5fc8b7bfdeaa18d5cadcf1ce5bff
Vasil Ninov <30572741+vaskoninov@users.noreply.github.com>
2023-05-23 06:38:43 +0000

    s

--- tryouts/while_loops.py
+++ tryouts/while_loops.py
@@ -3,12 +3,12 @@
 
 # while players < 3 or command != "e":
 #     command = input()
+
 #     players += 1
     
-
 # print(players)
 
-args = [("p", 1), ("s", 2)]
-products = [args[0] for args in args]
+# args = [("p", 1), ("s", 2)]
+# products = [args[0] for args in args]
 
-print(products)
+# print(products)
