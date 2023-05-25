57f48a5ee85c056d397da85686c764fe0f241362
vaskoninov <vasko.ninov@gmail.com>
2023-05-25 18:46:47 +0300

    new exercises

---
+++ May2023/error_handling.py
@@ -0,0 +1,31 @@
+#### So Many Exceptions
+# numbers_list = [int(x) for x in input().split(", ")]
+# result = 1
+#
+# for number in numbers_list:
+#     if number <= 5:
+#         result *= number
+#     elif 5 < number <= 10:
+#         result /= number
+# print(f"{result:.1f}")
+
+##### Repeat Text
+#
+# text = input()
+# while True:
+#     try:
+#         n = int(input())
+#         break
+#     except ValueError:
+#         print("Variable times must be an integer")
+# print(text * n)
+
+######
+class ValueCannotBeNegative(Exception):
+    pass
+
+
+for _ in range(5):
+    n = int(input())
+    if n < 0:
+        raise ValueCannotBeNegative
