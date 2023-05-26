f35b8ca92612ea3dca1a64c809d120d2626e6043
vaskoninov <vasko.ninov@gmail.com>
2023-05-26 21:46:14 +0300

    new exercises

--- May2023/Error_Handling_Homework/email_validator.py
+++ May2023/Error_Handling_Homework/email_validator.py
@@ -17,25 +17,38 @@ class TooManyAtSymbols(Exception):
     pass
 
 
+class NotAValidEmailAddress(Exception):
+    pass
+
+
+class NoUserNameProvided(Exception):
+    pass
+
+
 MIN_USER_NAME_LENGTH = 4
 VALID_DOMAINS = [".com", ".bg", ".org", ".net"]
 
-regex = re.compile(r'(?P<user>\w+)@\w+(?P<domain>\.\w+)')
+regex = re.compile(r'^(\w+)@(\w+\.)?\w+(\.\w+)$')
 
-email = input("Please enter your email address")
+email = input("Please enter your email address:\n")
 
 while email != "End":
-    matches = regex.finditer(email)
-    for match in matches:
-        user = match.group("user")
-        domain = match.group("domain")
+    if email.startswith("@"):
+        raise NoUserNameProvided("Email address should start with an user name")
+    matches = regex.findall(email)
+    if not matches:
+        raise NotAValidEmailAddress("Email address entered is not a valid one")
 
-        if len(user) < 4:
-            raise NameTooShortError("Name must be more than 4 characters")
-        if domain not in VALID_DOMAINS:
-            raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+    user = matches[0][0]
+    domain = matches[0][-1]
+
+    if len(user) < 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+    if domain not in VALID_DOMAINS:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
     if '@' not in email:
         raise MustContainAtSymbolError("Email must contain @")
     if email.count("@") > 1:
         raise TooManyAtSymbols("There should be one @ symbol")
-    email = input("Please enter your email address")
+    print("A valid email! Thank you!")
+    email = input("Please enter your email address:\n")
