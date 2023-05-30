00009a0fdbfb8b881c45378a53c8a0d612cd2c6f
vaskoninov <vasko.ninov@gmail.com>
2023-05-30 19:24:04 +0300

    new exercises

---
+++ May2023/File Handling/file_manipulator.py
@@ -0,0 +1,43 @@
+import os
+
+## Finds the path to a file
+abs_path = os.path.dirname(os.path.abspath(__file__))
+
+while True:
+    instructions = input()
+
+    ## Breaks the cycle
+    if instructions == "End":
+        break
+
+    ## Provides commands and data
+    command, file, *commands = instructions.split("-")
+    file_path = os.path.join(abs_path, file)
+
+    if command == "Create":
+        with open(file_path, "w"):
+            pass
+
+    if command == "Add":
+        content = commands
+        with open(file_path, "a") as f:
+            f.write(f"{content}\n")
+
+    if command == "Replace":
+        old_string, new_string = commands
+        try:
+            with open(file_path, "r+") as f:
+                text = f.read()
+                text = text.replace(old_string, new_string)
+
+                file.seek()
+                f.write(text)
+
+        except FileNotFoundError:
+            print("An error occurred")
+
+    if command == "Delete":
+        try:
+            os.remove(file_path)
+        except FileNotFoundError:
+            print("An error occurred")
