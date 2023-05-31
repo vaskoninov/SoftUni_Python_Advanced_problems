04f01a11609ac7cd575dc84cc8b73fcbcca2d0f8
vaskoninov <vasko.ninov@gmail.com>
2023-05-31 17:40:29 +0300

    new exercises

--- May2023/File_Handling/file_renaming.py
+++ May2023/File_Handling/file_renaming.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 directory = input("Enter a directory: ")
 to_replace = input("Enter symbol to be replaced: ")
@@ -7,3 +8,9 @@ replace_with = input("Enter symbol to replace with: ")
 for filename in os.listdir(directory):
     file = os.path.join(directory, filename)
     print(file)
+    if os.path.isfile(file):
+        new_name = filename.replace(to_replace, replace_with)
+        print(new_name)
+        new_file = '/'.join(re.split(r'[\\/]', file)[:-1]) + "/" + new_name
+        print(new_file)
+        os.rename(file, new_file)
