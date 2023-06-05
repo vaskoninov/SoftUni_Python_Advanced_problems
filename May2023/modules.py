06a3f49dcb4716cbe108dbe13460ee537acf3932
vaskoninov <vasko.ninov@gmail.com>
2023-06-05 20:10:58 +0300

    new

---
+++ May2023/modules.py
@@ -0,0 +1,34 @@
+# module -> python .py file
+
+# modules are stored in PACKAGES
+# a directory with __init__.py file plus .py files
+
+# library -> packages -> modules
+
+#### Calculate logarithm
+#
+# from math import log
+#
+# number = int(input())
+# base = input()
+# if base == "natural":
+#     print(f"{log(number):.2f}")
+# else:
+#     print(f"{log(number, int(base)):.2f}")
+
+##### External Modules and PIP
+#### requirements.txt
+
+# from pyfiglet import figlet_format
+#
+#
+# def print_art(msg):
+#     ascii_art = figlet_format(msg)
+#     print(ascii_art)
+#
+#
+# msg = input("What would you like to print? ")
+# print_art(msg)
+
+
+#### Custom Modules
