28056ae4cacd90daf2849add6bca98816a9291ca
vaskoninov <vasko.ninov@gmail.com>
2023-06-09 20:22:13 +0300

    alternative solution

--- May2023/Exam_Preparation/exam_preparation_1.py
+++ May2023/Exam_Preparation/exam_preparation_1.py
@@ -128,78 +128,79 @@
 
 
 #### Naughty or Nice
-from collections import Counter
+# from collections import Counter
+#
+#
+# def naughty_or_nice_list(sequence, *args, **kwargs):
+#     kids = {}
+#     result = {
+#         "Nice": [],
+#         "Naughty": [],
+#         "Not found": [],
+#     }
+#
+#     for num, kid in sequence:
+#         if num not in kids:
+#             kids[num] = []
+#         kids[num].append(kid)
+#     print(kids)
+#     commands = [arg.split('-') for arg in args]
+#
+#     for command in commands:
+#         x = int(command[0])
+#         disc = command[1]
+#         if x in kids:
+#             if len(kids[int(x)]) == 1:
+#                 name = kids[x][0]
+#                 result[disc].append(name)
+#                 sequence.remove((x, name))
+#
+#     counter = Counter(arg[1] for arg in sequence)
+#     for key, value in kwargs.items():
+#         if counter[key] == 1:
+#             result[value].append(key)
+#             for i, v in enumerate(sequence):
+#                 if v[1] == key:
+#                     sequence.pop(i)
+#
+#     for num, kid in sequence:
+#         # if kid not in result["Naughty"] and kid not in result["Nice"]:
+#         result["Not found"].append(kid)
+#
+#     final = []
+#     for key, value in result.items():
+#         if value:
+#             final.append(f"{key}: {', '.join(value)}")
+#     return "\n".join(final)
+#
+#
+# print(naughty_or_nice_list(
+#     [
+#         (3, "Amy"),
+#         (1, "Tom"),
+#         (7, "George"),
+#         (3, "Katy"),
+#     ],
+#     "3-Nice",
+#     "1-Naughty",
+#     Amy="Nice",
+#     Katy="Naughty",
+# ))
+#
+# print(naughty_or_nice_list(
+#     [
+#         (7, "Peter"),
+#         (1, "Lilly"),
+#         (2, "Peter"),
+#         (12, "Peter"),
+#         (3, "Simon"),
+#     ],
+#     "3-Nice",
+#     "5-Naughty",
+#     "2-Nice",
+#     "1-Nice",
+# ))
 
-
-def naughty_or_nice_list(sequence, *args, **kwargs):
-    kids = {}
-    result = {
-        "Nice": [],
-        "Naughty": [],
-        "Not found": [],
-    }
-
-    for num, kid in sequence:
-        if num not in kids:
-            kids[num] = []
-        kids[num].append(kid)
-    print(kids)
-    commands = [arg.split('-') for arg in args]
-
-    for command in commands:
-        x = int(command[0])
-        disc = command[1]
-        if x in kids:
-            if len(kids[int(x)]) == 1:
-                name = kids[x][0]
-                result[disc].append(name)
-                sequence.remove((x, name))
-
-    counter = Counter(arg[1] for arg in sequence)
-    for key, value in kwargs.items():
-        if counter[key] == 1:
-            result[value].append(key)
-            for i, v in enumerate(sequence):
-                if v[1] == key:
-                    sequence.pop(i)
-
-    for num, kid in sequence:
-        # if kid not in result["Naughty"] and kid not in result["Nice"]:
-        result["Not found"].append(kid)
-
-    final = []
-    for key, value in result.items():
-        if value:
-            final.append(f"{key}: {', '.join(value)}")
-    return "\n".join(final)
-
-
-print(naughty_or_nice_list(
-    [
-        (3, "Amy"),
-        (1, "Tom"),
-        (7, "George"),
-        (3, "Katy"),
-    ],
-    "3-Nice",
-    "1-Naughty",
-    Amy="Nice",
-    Katy="Naughty",
-))
-
-print(naughty_or_nice_list(
-    [
-        (7, "Peter"),
-        (1, "Lilly"),
-        (2, "Peter"),
-        (12, "Peter"),
-        (3, "Simon"),
-    ],
-    "3-Nice",
-    "5-Naughty",
-    "2-Nice",
-    "1-Nice",
-))
 
 # print(naughty_or_nice_list(
 #     [
@@ -219,3 +220,52 @@ print(naughty_or_nice_list(
 #     Merry="Nice",
 #     John="Naughty",
 # ))
+
+##### Naughty Or Nice 2
+
+def naughty_or_nice_list(santa_list, *args, **kwargs):
+    nice_kids, naughty_kids = [], []
+    result = []
+
+    def place_kid():
+        if len(kids) == 1:
+            nice_kids.append(kids[0][1]) if type_of_kid == "Nice" else naughty_kids.append(kids[0][1])
+            santa_list.remove(kids[0])
+
+    for kid_data in args:
+        number, type_of_kid = kid_data.split("-")
+        kids = [info for info in santa_list if info[0] == int(number)]
+        place_kid()
+
+    for name, type_of_kid in kwargs.items():
+        kids = [info for info in santa_list if info[1] == name]
+        place_kid()
+
+    if nice_kids:
+        result.append(f"Nice: {', '.join(nice_kids)}")
+    if naughty_kids:
+        result.append(f"Naughty: {', '.join(naughty_kids)}")
+    if santa_list:
+        result.append(f"Not found: {', '.join(k[1] for k in santa_list)}")
+
+    return "\n".join(result)
+
+
+print(naughty_or_nice_list(
+    [
+        (6, "John"),
+        (4, "Karen"),
+        (2, "Tim"),
+        (1, "Merry"),
+        (6, "Frank"),
+    ],
+    "6-Nice",
+    "5-Naughty",
+    "4-Nice",
+    "3-Naughty",
+    "2-Nice",
+    "1-Naughty",
+    Frank="Nice",
+    Merry="Nice",
+    John="Naughty",
+))
