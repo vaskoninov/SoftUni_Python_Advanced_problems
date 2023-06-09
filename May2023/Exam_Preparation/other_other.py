b905cfc85a37736c620691c46d05d2780fe39783
vaskoninov <vasko.ninov@gmail.com>
2023-06-09 17:49:23 +0300

    Update other_other.py

--- May2023/Exam_Preparation/other_other.py
+++ May2023/Exam_Preparation/other_other.py
@@ -44,7 +44,38 @@
 
 # Pawn Wars 4
 
-#
+# SIZE = 8
+
+# board = []
+# positions = [[], []]
+
+
+# def save_positions(search_for, index_to_save, r):
+#     if search_for in board[r]:
+#         positions[index_to_save].append(r)
+#         positions[index_to_save].append(board[r].index(search_for))
+
+
+# for row in range(SIZE):
+#     board.append(input().split())  # - - - - - b - => [-, -, -, -, -, b, -]
+
+#     save_positions("w", 0, row)
+#     save_positions("b", 1, row)
+
+# if abs(positions[0][1] - positions[1][1]) != 1 or positions[1][0] > positions[0][0]:
+#     if SIZE - positions[0][0] - 1 <= positions[1][0]:
+#         print(f"Game over! Black pawn is promoted to a queen at {chr(97 + positions[1][1])}1.")
+#     else:
+#         print(f"Game over! White pawn is promoted to a queen at {chr(97 + positions[0][1])}8.")
+# else:
+#     place = (positions[0][0] + positions[1][0]) // 2
+
+#     if positions[0][0] % 2 == positions[1][0] % 2:
+#         print(f"Game over! Black win, capture on {chr(97 + positions[0][1])}{SIZE - place}.")
+#     else:
+#         print(f"Game over! White win, capture on {chr(97 + positions[1][1])}{SIZE - place}.")
+
+
 # The Martian Rover
 
 
@@ -121,7 +152,7 @@ while bowls and customers:
 
     elif bowls[-1] > customers[0]:
         bowls[-1] -= customers.popleft()
-    
+
     elif bowls[-1] < customers[0]:
         customers[0] -= bowls.pop()
 
